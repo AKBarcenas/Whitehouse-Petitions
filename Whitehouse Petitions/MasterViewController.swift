d0e7459150aff33abd54760db6e955a9fcd60c14
AKBarcenas <akbarcen@ucsd.edu>
2015-12-29 22:43:51 -0800

    Added another tab and error handling

--- Whitehouse Petitions/MasterViewController.swift
+++ Whitehouse Petitions/MasterViewController.swift
@@ -18,7 +18,15 @@ class MasterViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        }
+        
+        else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
         
         if let url = NSURL(string: urlString) {
             if let data = try? NSData(contentsOfURL: url, options: []) {
@@ -27,7 +35,16 @@ class MasterViewController: UITableViewController {
                 if json["metadata"]["responseInfo"]["status"].intValue == 200 {
                     parseJSON(json)
                 }
+                else  {
+                    showError()
+                }
             }
+            else  {
+                showError()
+            }
+        }
+        else  {
+            showError()
         }
     }
     
@@ -41,6 +58,12 @@ class MasterViewController: UITableViewController {
         }
         
         tableView.reloadData()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
 
     override func viewWillAppear(animated: Bool) {
